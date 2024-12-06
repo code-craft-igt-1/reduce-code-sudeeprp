8eb0bdb68a23e2051ca65153a4f86aa09d9b5b59
Sudeep Prasad <sudeep.rp@gmail.com>
2024-12-06 16:29:51 +0530

    lambda experiment

---
+++ out/build/x64-Debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/ing02340/source/igt-icc/code-craft-igt-1/reduce-code-sudeeprp
+# Build directory: C:/Users/ing02340/source/igt-icc/code-craft-igt-1/reduce-code-sudeeprp/out/build/x64-Debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("C:/Users/ing02340/source/igt-icc/code-craft-igt-1/reduce-code-sudeeprp/out/build/x64-Debug/brightening-test[1]_include.cmake")
+subdirs("_deps/googletest-build")
